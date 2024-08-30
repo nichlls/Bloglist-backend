5f746f231f8f1b75840d191c081cc067786df196
nichlls <107227159+nichlls@users.noreply.github.com>
2024-08-30 15:09:20 +0100

    4.2

---
+++ app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+// const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const MONGODB_URI = config.MONGODB_URI
+
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.set('strictQuery', false)
+
+// logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
