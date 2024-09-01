a64e319a6506c0f9626f01f04a14ac5d32300c2c
nichlls <107227159+nichlls@users.noreply.github.com>
2024-09-01 21:40:29 +0100

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,6 +6,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  const mostLikes = blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max), blogs[0])
+
+  const favouriteBlog = {
+    title: mostLikes.title,
+    author: mostLikes.author,
+    likes: mostLikes.likes
+  }
+
+  return favouriteBlog
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favouriteBlog
 }
